f1f0bde4b58088a226c14010deb4c951b2ddf5b4
wakkame <wyuya0410@gmail.com>
2023-05-26 10:59:05 +0900

    #4 ISNMFの実装
    ISNMFの実装

--- ISNMF.m
+++ ISNMF.m
@@ -31,25 +31,39 @@ err = X - W_IS*H_IS;
 traceErr = sum(err.*err, 'all');
 cost_ISNMF(1) = sqrt(traceErr);
 
+% onesの事前準備
+ONE = ones(I,J);
+
 % IS-NMFの更新式
 for iItr = 1:nItr
-    W_IS = W_IS .* ( (X*H_IS.') ./ (W_IS*(H_IS*H_IS.')) );
-    H_IS = H_IS .* ( (W_IS.'*X) ./ ((W_IS.'*W_IS)*H_IS) );    
+    W_IS = W_IS.*((X./(W_IS*H_IS).^2 )*H_IS.' ./ (ONE./(W_IS*H_IS)*H_IS.')).^(0.5);
+    H_IS = H_IS.*(W_IS.'*(X./((W_IS*H_IS).^2)) ./ (W_IS.'*(ONE./(W_IS*H_IS)))).^(0.5);
 
     % Wの列毎に正規化係数を計算（列の総和）
-    % todo
-    nomalC = sum(W_IS,1);
+    nomalC_IS = sum(W_IS,1);
 
     % Wに正規化係数を適用
-    % todo
-    W_IS = W_IS ./ nomalC;
+    W_IS = W_IS ./ nomalC_IS;
        
     % Hに正規化係数を適用
-    % todo
-    H_KL = nomalC.' .*H_IS;
+    H_KL = nomalC_IS.' .*H_IS;
 
     % フロベニウスノルムの二乗値計算
     err = X - W_IS*H_IS;
     traceErr = sum(err.*err, 'all');
-    cost_EuNMF(iItr+1) = sqrt(traceErr);
+    cost_ISNMF(iItr+1) = sqrt(traceErr);
 end
+
+% 近似された観測行列の表示
+Xhat_IS = W_IS*H_IS;
+figure; imagesc(Xhat_IS);
+
+% コスト関数値のグラフ描画(線形)
+figure; plot(cost_ISNMF);
+xlabel("反復回数", "FontSize", 14);
+ylabel("コスト関数値(線形軸)", "FontSize", 14);
+
+% コスト関数値のグラフ描画(対数軸)
+figure; semilogy(cost_ISNMF);
+xlabel("反復回数", "FontSize", 14);
+ylabel("コスト関数値(対数軸)", "FontSize", 14);
