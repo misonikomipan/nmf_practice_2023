6f88c1e34705dba98c56310c9bc3deb68246baaf
wakkame <wyuya0410@gmail.com>
2023-06-09 14:04:42 +0900

    #14 STFTの関数化
    STFTを引数を音声ファイル名として関数化

--- stft.m
+++ stft.m
@@ -1,12 +1,25 @@
-%myvoiceの読み込み
-[y, fs] = audioread("myvoice.wav");
-y = ( y(:, 1) + y(:, 2) ) / 2;
+function [X] = STFT(filename, args)
+% STFT: 入力ファイルのSTFTを出力
+%   [Input]
+%       filename: 音声データのファイル名
+%       fftSize: fftの範囲
+%   [OutPut]
+%       X: 2次元実数非負観測行列
+%
+arguments
+    filename string
+    args.fftSize (1,1) double {mustBePositive, mustBeInteger} = 1024
+end
 
+fftSize = args.fftSize;
+
+%ファイルの読み込み
+[y, fs] = audioread(filename);
+if(size(y,2) == 2)
+    y = ( y(:, 1) + y(:, 2) ) / 2;
+end
 %信号長取得
 signalLength = size(y, 1);
-
-%fftSize定義
-fftSize = 1024;
 
 %shiftSize定義
 shiftSize = fftSize / 2;
@@ -52,6 +65,8 @@ ampSpec = sqrt(real(spec).^2 + imag(spec).^2);
 %db変換
 powerSpec = 20 * log10(ampSpec);
 
+X = powerSpec;
+
 %縦軸生成(周波数[Hz])
 %fsのfftsize分割
 yGrid = linspace(0,fs,fftSize);
@@ -67,7 +82,8 @@ axis xy;
 fontsize(gca, 15, "pixels")
 ylabel("周波数 [Hz]", 'FontSize', 18);
 xlabel("時間 [s]", 'FontSize', 18);
-caxis([-60 30]);
+caxis([-30 20]);
 ylim([0 fs/2]);
 colorbar;
 
+end
