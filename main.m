7db9d823486934feec88b76e5fa44d641930797e
wakkame <wyuya0410@gmail.com>
2023-06-10 19:24:01 +0900

    #14 音声データの近似
    STFTによる音声データの振幅スペクトログラム変換とこれを入力としたMNFによる近似

--- main.m
+++ main.m
@@ -1,23 +1,29 @@
 clear; close all; clc;
 
-% 行列のサイズ
-I = 100; % Xの行数
-J = 100; % Xの列数
-K = 10; % 基底数
+% 基底
+K = 10;
+
+% 音声ファイル
+audiofile = "kitamuravoice.wav";
+
+% 音声ファイルの振幅スペクトログラム変換(非負観測行列)
+X = STFT(audiofile);
+Nyquist = fix(size(X,1)/2) + 1;
+X = X(1:Nyquist,:);
+
+% 行列サイズの取得
+[I, J] = size(X);
 
 % パラメータ
 nItr = 1000; % 更新式の反復回数
 
-% 非負観測行列の生成
-trueW = rand(I, K); % 非負乱数（開区間(0, 1)）
-trueH = rand(K, J); % 非負乱数（開区間(0, 1)）
-X = trueW * trueH; % ランクKの非負観測行列
-
 % Xの表示
 figure; imagesc(X);
 
-[W_EU, H_EU] = NMF(X, K, "convergenceCurve",true,"nItr",nItr,"typeCostFunction","EU");
+[W, H] = NMF(X, K, "convergenceCurve",true,"nItr",nItr,"typeCostFunction","EU");
 
+%imagesc(W_EU,H_EU);
 % 近似された観測行列の表示
-Xhat_EU = W_EU*H_EU;
-figure; imagesc(Xhat_EU);
+Xhat = W*H;
+figure; imagesc(Xhat);
+
