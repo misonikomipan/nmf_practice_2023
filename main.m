12c55e1e6cfde89d4135f5da8201d98217f88300
wakkame <wyuya0410@gmail.com>
2023-06-23 11:47:27 +0900

    #16 分離音の複製
    ISTFTによる分離音声の復元

--- main.m
+++ main.m
@@ -3,47 +3,50 @@ clear; close all; clc;
 % 基底
 K = 2;
 
-% 音声ファイル
+% 音声ファイルの読み込み
 audiofile = "sound.wav";
+[y, fs] = audioread(audiofile);
+if(size(y,2) == 2)
+    y = ( y(:, 1) + y(:, 2) ) / 2;
+end
 
-% 音声ファイルの振幅スペクトログラム変換(非負観測行列)
-[X, fs] = STFT(audiofile, "fftSize", 2048);
-Nyquist = fix(size(X,1)/2) + 1; % ナイキスト周波数にあたるデータ位置
-X = X(1:Nyquist,:); % ナイキスト周波数までを抽出
+% DGTtoolのインスタンス生成
+F = DGTtool("windowShift", 1024, "windowLength", 2048, "FFTnum", 2048, "windowName", "blackman");
+
+% 音声ファイルのSTFT
+X = F(y);
+Y = STFT(audiofile, "fftSize", 2048);
 
 % 行列サイズの取得
 [I, J] = size(X);
 
+Nyquist = fix(I/2) + 1; % ナイキスト周波数にあたるデータ位置
+X = X(1:Nyquist,:); % ナイキスト周波数までを抽出
+
+% 振幅スペクトルに変換
+ampX = abs(X);
+
+% 位相スペクトルに変換
+phaseX = angle(X);
+
 % パラメータ
 nItr = 1000; % 更新式の反復回数
 
-[W, H] = NMF(X, K, "convergenceCurve",true,"nItr",nItr,"typeCostFunction","KL");
+[W, H] = NMF(ampX, K, "convergenceCurve",true,"nItr",nItr,"typeCostFunction","KL");
+
+% 分離振幅スペクトル
+Xhat1 = W(:,1)*H(1,:);
+Xhat2 = W(:,2)*H(2,:);
+
+% 分離複素スペクトル
+cpxXhat1 = Xhat1 .* exp(1i*phaseX);
+cpxXhat2 = Xhat2 .* exp(1i*phaseX);
+
+% 分離音声の復元
+sigXhat1 = F.pinv(cpxXhat1);
+sigXhat2 = F.pinv(cpxXhat2);
 
 % 分解音の表示
-% 縦軸を0からナイキスト周波数までI個に分割
-yGrid = linspace(0, fs/2, I);
-% numRow=(signalLength - fftSize)/shiftSize+1
-% numRow=J,fftSize=2*shiftSize=I
-% 再生時間=signalLength/fs, 以上から再生時間算出
-% 横軸として再生時間をJ個に分割
-xGrid = linspace(0, ((I+1)*J)/(fs), J);
-colorlim = [0, 80];
-ylimit = [0, 10000];
-figure; imagesc(xGrid, yGrid, W(:,1)*H(1,:));
-axis xy;
-fontsize(gca, 12, "pixels");
-ylabel("周波数 [Hz]");
-xlabel("時間 [s]");
-caxis(colorlim);
-ylim(ylimit);
-colorbar;
-
-figure; imagesc(xGrid, yGrid, W(:,2)*H(2,:));
-axis xy;
-fontsize(gca, 12, "pixels");
-ylabel("周波数 [Hz]");
-xlabel("時間 [s]");
-caxis(colorlim);
-ylim(ylimit);
-colorbar;
-
+figure; plot(y);
+figure; plot(sigXhat1);
+figure; plot(sigXhat2);
