013e618ca28a15356a9bf0ecea8bae3174769142
wakkame <wyuya0410@gmail.com>
2023-07-14 15:05:47 +0900

    欠損値ありNMF
    一度にすべてのNMFを実行し，結果を比較するように変更

--- step5.m
+++ step5.m
@@ -18,13 +18,35 @@ X = x1 * x2;
 losX = X;
 losX(randperm(nRow * nCol, nNan)) = nan;
 
-[W, H] = NMFforMissData(losX,rank,"convergenceCurve",true,"typeCostFunction","KL");
+[W_EU, H_EU] = NMFforMissData(losX,rank,"typeCostFunction","EU");
+[W_KL, H_KL] = NMFforMissData(losX,rank,"typeCostFunction","KL");
+[W_IS, H_IS] = NMFforMissData(losX,rank,"typeCostFunction","IS");
 
 % 近似された観測行列の表示
-Xhat = W * H;
+Xhat_EU = W_EU * H_EU;
+Xhat_KL = W_KL * H_KL;
+Xhat_IS = W_IS * H_IS;
+
 figure; imagesc(X);
 title("完全な観測行列");
-figure; imagesc(Xhat);
-title("近似行列");
-figure; imagesc(abs(X-Xhat));
-title("誤差[abs(X - Xhat)]");
+
+figure; imagesc(losX);
+title("欠損値あり観測行列");
+
+figure; imagesc(Xhat_EU);
+title("近似行列 EUNMF");
+
+figure; imagesc(abs(Xhat_EU-X)./X*100);
+title("誤差率 EUNMF");
+
+figure; imagesc(Xhat_KL);
+title("近似行列 KLNMF");
+
+figure; imagesc(abs(Xhat_KL-X)./X*100);
+title("誤差率 KLNMF");
+
+figure; imagesc(Xhat_IS);
+title("近似行列 ISNMF");
+
+figure; imagesc(abs(Xhat_IS-X)./X*100);
+title("誤差率 ISNMF");
