921e82af9d8740b53b2c451977aa64da27938a7c
wakkame <wyuya0410@gmail.com>
2023-07-19 11:45:40 +0900

    欠損値ありNMFの実装
    グラフタイトルの書式変更
    検証の指標として誤差平均と誤差の標準偏差を導出

--- step5.m
+++ step5.m
@@ -34,19 +34,44 @@ figure; imagesc(losX);
 title("欠損値あり観測行列");
 
 figure; imagesc(Xhat_EU);
-title("近似行列 EUNMF");
+t = 'EUNMF $\hat{X}_{EU}$';
+title(t,'interpreter','latex');
 
 figure; imagesc(abs(Xhat_EU-X)./X*100);
-title("誤差率 EUNMF");
+t = 'EUNMF $|\hat{X}_{EU}|\otimes X^{-1} \times 100$';
+title(t,'interpreter','latex');
 
 figure; imagesc(Xhat_KL);
-title("近似行列 KLNMF");
+t = 'KLNMF $\hat{X}_{KL}$';
+title(t,'interpreter','latex');
 
 figure; imagesc(abs(Xhat_KL-X)./X*100);
-title("誤差率 KLNMF");
+t = 'KLNMF $|\hat{X}_{KL}|\otimes X^{-1} \times 100$';
+title(t,'interpreter','latex');
 
 figure; imagesc(Xhat_IS);
-title("近似行列 ISNMF");
+t = 'ISNMF $\hat{X}_{IS}$';
+title(t,'interpreter','latex');
 
 figure; imagesc(abs(Xhat_IS-X)./X*100);
-title("誤差率 ISNMF");
+t = 'ISNMF $|\hat{X}_{IS}|\otimes X^{-1} \times 100$';
+title(t,'interpreter','latex');
+
+% ---精度評価---
+% 誤差平均(絶対値)
+errAVG_EU = sum(abs(Xhat_EU-X),"all")/(nRow*nCol);
+errAVG_KL = sum(abs(Xhat_KL-X),"all")/(nRow*nCol);
+errAVG_IS = sum(abs(Xhat_IS-X),"all")/(nRow*nCol);
+
+% 誤差(絶対値)の標準偏差
+errDev_EU = (sum((abs(Xhat_EU-X)-errAVG_EU)^2,'all')/(nRow*nCol))^(0.5);
+errDev_KL = (sum((abs(Xhat_KL-X)-errAVG_KL)^2,'all')/(nRow*nCol))^(0.5);
+errDev_IS = (sum((abs(Xhat_IS-X)-errAVG_IS)^2,'all')/(nRow*nCol))^(0.5);
+
+errAVG = [errAVG_EU,errAVG_KL,errAVG_IS];
+errDev = [errDev_EU,errDev_KL,errDev_IS];
+
+figure;bar(errAVG);
+title("誤差平均:1-EU,2-KL,3-IS");
+figure;bar(errDev);
+title("誤差の標準偏差:1-EU,2-KL,3-IS");
